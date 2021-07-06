23aebbd2c4627c6ca99d7f22bf34016e8f608ca0
DONSIMON92 <semenische@yandex.com>
2021-07-06 13:08:35 +0000

    add mypy, black and flake8-black

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cd /usr/src/app
+
 echo "Installing dependencies from Pipfile.lock"
 pipenv sync
 echo "Done"
+
 echo "Running project"
 python3 -m /usr/src/app/organizer
+echo "Done"
+
 exec "$@"
